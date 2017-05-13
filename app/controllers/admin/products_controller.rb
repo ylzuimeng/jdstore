cd59341ced940527edfbc3c0979ba1df44e66fc5
ylzuimeng <ylzuimeng@gmail.com>
2017-05-13 09:05:29 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,7 +38,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :desription, :quantity, :price )
+    params.require(:product).permit(:title, :desription, :quantity, :price, :image )
   end
 
 end
