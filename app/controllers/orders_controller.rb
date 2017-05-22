5d3f7064f2fbde85e544b0811ab80c15cf101cd6
ylzuimeng <ylzuimeng@gmail.com>
2017-05-23 07:23:30 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,8 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
 
       redirect_to order_path(@order.token)
     else
@@ -28,7 +30,7 @@ class OrdersController < ApplicationController
     @product_lists = @order.product_lists
   end
 
-  def pay_wiht_alipay
+  def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
     @order.pay!
@@ -43,7 +45,7 @@ class OrdersController < ApplicationController
     @order.pay!
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
-    
+
   end
 
   private
--- app/models/order.rb
+++ app/models/order.rb
@@ -14,11 +14,11 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   def set_payment_with!(method)
-    self.update_column(payment_method: method)
+    self.update_columns(payment_method: method)
 
   end
 
   def pay!
-    self.update_column(is_paid: true )
+    self.update_columns(is_paid: true )
   end
 end
