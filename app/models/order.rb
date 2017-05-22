376ab0bb20f7ea95a89e5660adb9800f22384fa9
ylzuimeng <ylzuimeng@gmail.com>
2017-05-22 11:11:48 +0800

    add payment_method function

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,4 +12,13 @@ class Order < ApplicationRecord
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+  def set_payment_with!(method)
+    self.update_column(payment_method: method)
+
+  end
+
+  def pay!
+    self.update_column(is_paid: true )
+  end
 end
